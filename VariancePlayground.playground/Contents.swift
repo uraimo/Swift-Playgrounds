7927771fdcb0906d2723f3ed615308359803f707
Umberto Raimondi <me@uraimo.com>
2015-09-29 12:04:29 +0200

    Added Playground for the function types conversion post

---
+++ VariancePlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+// See original post at http://www.uraimo.com/2015/09/29/Swift2.1-Functions-Covariance-Contravariance/
+
+func testVariance(foo:(Int)->Any){foo(1)}
+
+func innerAnyInt(p1:Any) -> Int{ return 1 }
+func innerAnyAny(p1:Any) -> Any{ return 1 }
+func innerIntInt(p1:Int) -> Int{ return 1 }
+func innerIntAny(p1:Int) -> Any{ return 1 }
+
+testVariance(innerIntAny)
+testVariance(innerAnyInt)
+testVariance(innerAnyAny)
+testVariance(innerIntInt)
+
+ 
