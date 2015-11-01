5485d85fbfdcf43badbad26ccd5b5d2c08bee086
Umberto Raimondi <me@uraimo.com>
2015-11-01 17:17:56 +0100

    New playground for the error handling article

---
+++ 2015-11-Swift2ErrorHandlingPlayground.playground/Contents.swift
@@ -0,0 +1,82 @@
+//: Playground - noun: a place where people can play
+//
+// See original post at http://www.uraimo.com/2015/10/29/error-handling-from-objective-c-to-swift-2-and-back/
+
+/////////////////////////////////////
+//Simple Example
+enum MyError : ErrorType{
+    case AnError
+    case AnotherError
+    case JustAnotherError
+}
+
+func throwsError()throws ->Int {
+    throw MyError.AnotherError
+}
+
+do{
+    try throwsError()
+}catch MyError.AnError {
+    print("AnError")
+}catch MyError.AnotherError {
+    print("AnotherError")  //AnotherError will be catched and printed
+}catch{
+    print("Something else happened")
+}
+
+do{
+    do{
+        try throwsError()
+    }catch MyError.AnError {
+        print("AnError")
+    }
+}catch MyError.AnotherError {
+    print("AnotherError")  //AnotherError will be catched and printed
+}catch{
+    print("Something else happened")
+}
+
+
+/////////////////////////////////////
+// More complex example
+
+enum MyError2 : ErrorType{
+    case GenericError
+    case DetailedError(String)
+    case NumericError(Int)
+}
+
+func throwsDetailedError()throws ->Int {
+    throw MyError2.DetailedError("Some details here")
+}
+
+func shouldNeverThrow()throws ->Int {
+    return 0
+}
+
+do{
+    defer{
+        //Clean up
+    }
+    
+    try throwsDetailedError()
+    var value = try! shouldNeverThrow()
+    var imNil = try? throwsDetailedError()
+}catch MyError2.GenericError {
+    print("GenericError")
+}catch MyError2.DetailedError(let message) {
+    print("Error: \(message)")  //Will print Error: Some details here
+}catch MyError2.NumericError(let number) where number>0{
+    print("Error with id: "+String(number))
+}catch{
+    print("Something else happened: "+String(error))
+}
+
+
+
+var convertedInt = (try? shouldNeverThrow()).map{String($0)}
+convertedInt
+
+
+
+
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 A collection of playgrounds used in my posts:
 
+* November 2015: [Swift 2.1 Error Handling: from Objective-C to Swift and Back](http://www.uraimo.com/2015/11/03/error-handling-from-objective-c-to-swift-2-and-back/)
 * October 2015: [Swift 2.0: Map and FlatMap Demystified](http://www.uraimo.com/2015/10/08/Swift2-map-flatmap-demystified/)
 * September 2015: [Swift 2.1 Function Types Conversion: Covariance and Contravariance](http://www.uraimo.com/2015/09/29/Swift2.1-Function-Types-Conversion-Covariance-Contravariance/)
