62f6a43f6eb0368792cf867ca9b0c222e1cdd05e
Umberto Raimondi <me@uraimo.com>
2015-11-12 17:44:34 +0100

    SequenceType Playground

---
+++ 2015-11-12-SequenceTypeGeneratorTypePlayground.playground/Contents.swift
@@ -0,0 +1,180 @@
+
+//: ### Experimenting with Swift Sequences and Generators
+//: ---
+//:
+//: Read the original post at [http://www.uraimo.com/2015/11/12/experimenting-with-swift-2-sequencetype-generatortype/](http://www.uraimo.com/2015/11/12/experimenting-with-swift-2-sequencetype-generatortype/)
+
+
+
+//:Let's build a simple generator that produces numbers from the well known Fibonacci sequence:
+class FibonacciGenerator : GeneratorType {
+    var last = (0,1)
+    var endAt:Int
+    var lastIteration = 0
+    
+    init(end:Int){
+        endAt = end
+    }
+    
+    func next() -> Int?{
+        guard lastIteration<endAt else {
+            return nil
+        }
+        lastIteration++
+        
+        let next = last.0
+        last = (last.1,last.0+last.1)
+        return next
+    }
+}
+
+//: To return a finite sequence we need an additional constructor that we'll use to specify the sequence length and return *nil* instead of a new element when we reach it. There is not much else to see here other than the tuple swap trick that save us a few lines, but let's see how to use this generator: 
+
+var fg = FibonacciGenerator(end:10)
+
+while let fib = fg.next() {
+    print(fib)
+}
+
+
+// :This way we'll iterate on the generator elements until *nil* is returned.
+    
+//: Implementing a **SequenceType** for this generator is straightforward:
+
+class FibonacciSequence : SequenceType {
+    var endAt:Int
+    
+    init(end:Int){
+        endAt = end
+    }
+    
+    func generate() -> FibonacciGenerator{
+        return FibonacciGenerator(end: endAt)
+    }
+}
+
+let arr = Array(FibonacciSequence(end:10))
+
+for f in FibonacciSequence(end: 10) {
+    print(f)
+}
+
+
+//: But there is no need to declare the generator as a separated entity, we can use the **anyGenerator** utility method with the **AnyGenerator<T>** class to make this example more compact: 
+
+class CompactFibonacciSequence : SequenceType {
+    var endAt:Int
+    
+    init(end:Int){
+        endAt = end
+    }
+    
+    func generate() -> AnyGenerator<Int> {
+        var last = (0,1)
+        var lastIteration = 0
+        
+        return anyGenerator({
+            guard lastIteration<self.endAt else {
+                return nil
+            }
+            lastIteration++
+            
+            let next = last.0
+            last = (last.1,last.0+last.1)
+            return next
+        })
+    }
+}
+
+for f in CompactFibonacciSequence(end: 10) {
+    print(f)
+}
+
+//: In some circumstances, a simple sequence generated with **anyGenerator()** could be more than enough for what we want to do.
+//: Let's create a sequence with the first 10 numbers of the [Lucas sequence](https://en.wikipedia.org/wiki/Lucas_number) (clearly not *that* Lucas), a numeric series similar to Fibonacci sequence that starts with *2,1* instead of *0,1*, using a generator and initialize an array with it:   
+
+var last = (2,1)
+var c = 0
+
+let lucas = anyGenerator{
+    ()->Int? in
+    guard c<10 else {
+        return nil
+    }
+    
+    c++
+    let next = last.0
+    last = (last.1,last.0+last.1)
+    return next
+}
+
+let a = Array(lucas) //[2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
+
+
+//: Definitely not bad, we removed a lot of boilerplate, but since we can improve our algorithm further with a formula involving the [golden ratio](https://en.wikipedia.org/wiki/Golden_ratio), let's do it:
+
+import Darwin
+
+let Phi = (sqrt(5)+1.0)/2
+let phi = 1/Phi
+
+func luc(n:Int)->Int {
+    return Int(pow(Phi, Double(n))+pow(-phi,Double(n)))
+}
+
+c = 0
+var compactLucas = anyGenerator{ c<10 ? luc(c++): nil }
+
+let a2 = Array(compactLucas) //[2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
+
+//: To try out some of the functional(ish) facilities that **SequenceType** provide, we'll now build a derived sequence that will only return *even* numbers from the Lucas sequence:
+
+c = 0
+var evenCompactLucas = anyGenerator{ c<10 ? luc(c++): nil }.filter({$0 % 2 == 0})
+
+let a3 = Array(evenCompactLucas) //[2, 4, 18, 76]
+
+//: But now, what it we remove the nil termination requirement described above to build an infinite sequence of all the possible Lucas numbers?
+
+c = 0
+var infiniteLucas = anyGenerator{luc(c++)}
+
+
+let a4 = Array(infiniteLucas.prefix(10)) //[2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
+
+for var f in infiniteLucas.prefix(10){
+    print(f)
+}
+
+//: But let's go a step further and again apply a *filter* to our sequence, to obtain a sequence of even Lucas numbers:
+//:
+//: Remove ".lazy" and grab a coffee...
+var onlyEvenLucas = infiniteLucas.lazy.filter({$0 % 2 == 0})
+for var f in onlyEvenLucas.prefix(10){
+    print(f)
+}
+
+
+//: Let's see visually what's happening if we remove *.lazy* using a more verbose infinite sequence of integers that will print some text every time a value is requested from the generator:
+class InfiniteSequence :SequenceType {
+    func generate() -> AnyGenerator<Int> {
+        var i = 0
+        return anyGenerator({
+            print("# Returning "+String(i))
+            return i++
+        })
+    }
+}
+
+//: Again, remove ".lazy" and grab a coffee...
+var fs = InfiniteSequence().lazy.filter({$0 % 2 == 0}).generate()
+
+for i in 1...5 {
+    print(fs.next())
+}
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A collection of playgrounds used in my posts:
 
-* November 2015: [Swift 2.1 Error Handling: from Objective-C to Swift and Back](http://www.uraimo.com/2015/11/03/error-handling-from-objective-c-to-swift-2-and-back/)
-* October 2015: [Swift 2.0: Map and FlatMap Demystified](http://www.uraimo.com/2015/10/08/Swift2-map-flatmap-demystified/)
-* September 2015: [Swift 2.1 Function Types Conversion: Covariance and Contravariance](http://www.uraimo.com/2015/09/29/Swift2.1-Function-Types-Conversion-Covariance-Contravariance/)
+* 12 November 2015: [Experimenting with Swift Sequences and Generators](http://www.uraimo.com/2015/11/12/experimenting-with-swift-2-sequencetype-generatortype/)
+* 3 November 2015: [Swift 2.1 Error Handling: from Objective-C to Swift and Back](http://www.uraimo.com/2015/11/03/error-handling-from-objective-c-to-swift-2-and-back/)
+* 8 October 2015: [Swift 2.0: Map and FlatMap Demystified](http://www.uraimo.com/2015/10/08/Swift2-map-flatmap-demystified/)
+* 29 September 2015: [Swift 2.1 Function Types Conversion: Covariance and Contravariance](http://www.uraimo.com/2015/09/29/Swift2.1-Function-Types-Conversion-Covariance-Contravariance/)
