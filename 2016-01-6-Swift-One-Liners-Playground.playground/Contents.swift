733f0ff3f94427ae8dadd705cb2cfe0be423f72e
Umberto Raimondi <me@uraimo.com>
2016-09-02 10:03:31 +0200

    Updated One Liners post to Swift 3

--- 2016-01-6-Swift-One-Liners-Playground.playground/Contents.swift
+++ 2016-01-6-Swift-One-Liners-Playground.playground/Contents.swift
@@ -16,7 +16,7 @@ import Foundation
 //:#2 Sum a list of numbers
 
 
-(1...1024).reduce(0,combine: +)
+(1...1024).reduce(0,+)
 
 
 //:#3 Verify if Exists in a String
@@ -25,27 +25,29 @@ import Foundation
 let words = ["Swift","iOS","cocoa","OSX","tvOS"]
 let tweet = "This is an example tweet larking about Swift"
 
-let valid = !words.filter({tweet.containsString($0)}).isEmpty
+let valid = !words.filter({tweet.contains($0)}).isEmpty
 valid //true
 
 //Or alternatively, as suggested by @oisdk
 
-let valid2 = words.contains(tweet.containsString)
+let valid2 = words.contains(where:tweet.contains)
 valid2 //true
 
 let valid3 = tweet.characters
-            .split(" ")
-            .lazy
-            .map(String.init)
-            .contains(Set(words).contains)
+                .split(separator:" ")
+                .lazy
+                .map(String.init)
+                .contains(where:Set(words).contains)
 valid3 //true
 
 //:#4 Read in a File
 
 
-let path = NSBundle.mainBundle().pathForResource("test", ofType: "txt")
+let path = Bundle.main.path(forResource:"test", ofType: "txt")
+let lines = try? String(contentsOfFile: path!).characters
+                     .split{$0 == "\n"}
+                     .map(String.init)
 
-let lines = try? String(contentsOfFile: path!).characters.split{$0 == "\n"}.map(String.init)
 if let lines=lines {
     lines[0] // O! for a Muse of fire, that would ascend
     lines[1] // The brightest heaven of invention!
@@ -64,40 +66,43 @@ let name = "uraimo"
 //:#6 Filter list of numbers
 
 //Obvious implementation
-extension SequenceType{
-    typealias Element = Self.Generator.Element
+extension Sequence{
+   typealias Element = Self.Iterator.Element 
     
-    func partitionBy(fu: (Element)->Bool)->([Element],[Element]){
-        var first=[Element]()
-        var second=[Element]()
-        for el in self {
-            if fu(el) {
-                first.append(el)
-            }else{
-                second.append(el)
-            }
-        }
-        return (first,second)
-    }
-}
+   func partitionBy(fu: (Element)->Bool)->([Element],[Element]){
+       var first=[Element]() 
+       var second=[Element]() 
+       for el in self {
+          if fu(el) { 
+             first.append(el) 
+          }else{ 
+             second.append(el) 
+          } 
+       } 
+       return (first,second) 
+   } 
+} 
 
 let part = [82, 58, 76, 49, 88, 90].partitionBy{$0 < 60}
 part // ([58, 49], [82, 76, 88, 90])
 
 //Slightly improved implementation
-extension SequenceType{
-    
-    func anotherPartitionBy(fu: (Self.Generator.Element)->Bool)->([Self.Generator.Element],[Self.Generator.Element]){
-        return (self.filter(fu),self.filter({!fu($0)}))
-    }
-}
+extension Sequence{ 
+   func anotherPartitionBy(fu: (Self.Iterator.Element)->Bool)->
+          ([Self.Iterator.Element],[Self.Iterator.Element]){ 
+      return (self.filter(fu),self.filter({!fu($0)})) 
+   } 
+} 
 
 let part2 = [82, 58, 76, 49, 88, 90].anotherPartitionBy{$0 < 60}
 part2 // ([58, 49], [82, 76, 88, 90])
 
 
 //This is better, but needs type hints to make it easier to evaluate or it will trigger a complexity error
-var part3 = [82, 58, 76, 49, 88, 90].reduce( ([],[]), combine: { (a:([Int],[Int]),n:Int) -> ([Int],[Int]) in (n<60) ?(a.0+[n],a.1):(a.0,a.1+[n]) })
+var part3 = [82, 58, 76, 49, 88, 90].reduce( ([],[]), { 
+   (a:([Int],[Int]),n:Int) -> ([Int],[Int]) in 
+   (n<60) ? (a.0+[n],a.1) : (a.0,a.1+[n])  
+}) 
 part3 // ([58, 49], [82, 76, 88, 90])
 
 
@@ -106,7 +111,7 @@ part3 // ([58, 49], [82, 76, 88, 90])
 
 //Uncomment to enable, will reload on every modification
 /*
-let xmlDoc = try? AEXMLDocument(xmlData: NSData(contentsOfURL: NSURL(string:"https://www.ibiblio.org/xml/examples/shakespeare/hen_v.xml")!)!)
+let xmlDoc = try? AEXMLDocument(xmlData: NSData(contentsOf: URL(string:"https://www.ibiblio.org/xml/examples/shakespeare/hen_v.xml")!)!) 
 
 if let xmlDoc=xmlDoc {
     var prologue = xmlDoc.root.children[6]["PROLOGUE"]["SPEECH"]
@@ -121,15 +126,15 @@ if let xmlDoc=xmlDoc {
 //:#8 Find minimum (or maximum) in a List
 
 
-//Find the minimum of an array of Ints
-[10,-22,753,55,137,-1,-279,1034,77].sort().first
-[10,-22,753,55,137,-1,-279,1034,77].reduce(Int.max, combine: min)
-[10,-22,753,55,137,-1,-279,1034,77].minElement()
+//Find the minimum of an array of Ints 
+[10,-22,753,55,137,-1,-279,1034,77].sorted().first 
+[10,-22,753,55,137,-1,-279,1034,77].reduce(Int.max, min) 
+[10,-22,753,55,137,-1,-279,1034,77].min()
 
-//Find the maximum of an array of Ints
-[10,-22,753,55,137,-1,-279,1034,77].sort().last
-[10,-22,753,55,137,-1,-279,1034,77].reduce(Int.min, combine: max)
-[10,-22,753,55,137,-1,-279,1034,77].maxElement()
+//Find the maximum of an array of Ints 
+[10,-22,753,55,137,-1,-279,1034,77].sorted().last 
+[10,-22,753,55,137,-1,-279,1034,77].reduce(Int.min, max) 
+[10,-22,753,55,137,-1,-279,1034,77].max()
 
 
 //:#9 Parallel Processing
@@ -144,14 +149,20 @@ if let xmlDoc=xmlDoc {
 var n = 50
 var primes = Set(2...n)
 
-(2...Int(sqrt(Double(n)))).forEach{primes.subtractInPlace((2*$0).stride(through:n, by:$0))}
-primes.sort()
+var n = 50 
+var primes = Set(2...n) 
+(2...Int(sqrt(Double(n)))).forEach{
+     primes.subtract(Set(stride(from:2*$0, to:n, by:$0)))
+}
+primes.sorted()
+
 
 // Without Side-effects
-var sameprimes = Set(2...n)
+var sameprimes = Set(2...n) 
 
-sameprimes.subtractInPlace((2...Int(sqrt(Double(n)))).flatMap{ (2*$0).stride(through:n, by:$0)})
-sameprimes.sort()
+sameprimes.subtract(Set(2...Int(sqrt(Double(n)))) 
+                   .flatMap{stride(from:2*$0, to:n, by:$0)}) 
+sameprimes.sorted()
 
 
 //:#11 Bonus: Tuple swap via destructuring
@@ -162,10 +173,5 @@ var a=1,b=2
 (a,b) = (b,a)
 a
 b
-
-
-
-
-
 
 
